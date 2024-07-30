b8a98627e21944e629f6f86f2d7fa3df7aa7a5b0
Lam Kha <builamkha004@gmail.com>
2024-07-30 16:46:59 +0700

    createTodoList

---
+++ script.js
@@ -0,0 +1,95 @@
+const $= document.querySelector.bind(document);
+
+const inputBox= $('.inputFiled input');
+const addBox= $('.inputFiled button');
+const todoList= $('.todoList');
+const deleteAllBtn= $('.footer button');
+
+let listArr=[];
+inputBox.onkeyup = ()=>{
+    //lấy giá trị khi user nhập vào ô input
+    let userEnterValue= inputBox.value;
+    if(userEnterValue.trim() != 0){
+        addBox.classList.add('active');
+    }else{
+        addBox.classList.remove('active');
+    }
+}
+
+addBox.onclick=()=>{
+    // lấy giá trị của ô input lưu vào userEnterValue
+    let userEnterValue= inputBox.value;
+
+    let getLocalStorageData = localStorage.getItem('New todo') ;
+    if(getLocalStorageData == null){
+        listArr=[];
+    }else{
+        listArr= JSON.parse(getLocalStorageData);
+    }
+    listArr.push(userEnterValue);
+    localStorage.setItem('New todo',JSON.stringify(listArr));
+
+    showTasks();
+
+    addBox.classList.remove('active');
+}  
+
+    function showTasks(){
+        let getLocalStorageData = localStorage.getItem("New todo") ;
+        if(getLocalStorageData == null){
+            listArr=[];
+        }else{
+            listArr= JSON.parse(getLocalStorageData);
+        }
+        const pendingTaskNumb= document.querySelector('.pendingTask');
+        pendingTaskNumb.textContent = listArr.length;
+
+
+        if(listArr.length > 0){
+            deleteAllBtn.classList.add('active');
+        }else{
+            deleteAllBtn.classList.remove('active')
+        }
+
+        let newLiTag= "";
+        listArr.forEach((element, index) => {
+            newLiTag += `<div class="content"><li>
+            ${index+1}.
+            ${element}</li><span class="icon"  data-index="${index}"> <i class="fas fa-trash"></i>
+            <span class="remove-text">xóa</span>
+            </span></div>`;
+        });
+
+        todoList.innerHTML = newLiTag;
+        inputBox.value="";
+
+        document.querySelectorAll('.icon').forEach(item =>{
+            item.addEventListener('click',function(){
+                deleteTask(item.getAttribute('data-index'))
+            })
+        })
+
+}
+
+function deleteTask(index){
+    let getLocalStorageData=localStorage.getItem("New todo");
+    if(getLocalStorageData){
+        listArr =JSON.parse(getLocalStorageData);
+        listArr.splice(index,1);
+        localStorage.setItem("New todo",JSON.stringify(listArr));
+        showTasks();
+    }
+}
+deleteAllBtn.onclick=()=>{
+    listArr=[];
+    localStorage.setItem("New todo",JSON.stringify(listArr));
+    showTasks();
+}
+inputBox.addEventListener('keydown', function(event){
+    if(event.key==='Enter'){
+        addBox.onclick();
+    }
+})
+showTasks();
+
+
